bde03832fb358ace8fec5c815f91df1da7578f98
haj amir <amirfattahi22@gmail.com>
2024-07-24 17:21:59 +0330

    slider

--- src/components/main/slide-swiper/StorySlide.tsx
+++ src/components/main/slide-swiper/StorySlide.tsx
@@ -80,8 +80,10 @@ const slide: Array<Slide> = [
 ];
 function StorySlide() {
   return (
-    <>
-      <div className="max-w-[1336px] w-screen  flex h-[150px] pt-5 flex-row  mx-auto justify-center items-center text-center align-middle storySlide">
+    <div>
+      <div
+        className="max-w-[1336px] w-screen  flex h-[150px] pt-5 flex-row  mx-auto justify-center items-center text-center align-middle storySlide"
+      >
         <Swiper
           dir="rtl"
           slidesPerView={13}
@@ -120,7 +122,7 @@ function StorySlide() {
           ))}
         </Swiper>
       </div>
-    </>
+    </div>
   );
 }
 
