a2d776e9effea6f6682ee283da6f14cebeb4052a
haj amir <amirfattahi22@gmail.com>
2024-07-24 18:44:05 +0330

    slider

--- src/components/main/Homepage.tsx
+++ src/components/main/Homepage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import StorySlide from "./slide-swiper/StorySlide";
 import BigestSlide from "./slide-swiper/BigestSlide";
+import DigiJet from "./DigiJet";
 
 function Homepage() {
   return (
     <div className="flex flex-col gap-y-4 items-center max-w-[1920px] overflow-hidden justify-center mx-auto">
       <StorySlide />
-      <BigestSlide/>
+      <BigestSlide />
+      <DigiJet/>
     </div>
   );
 }
